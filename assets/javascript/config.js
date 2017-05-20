5c5b4cf9719cd4766e4405032926c81053b2ca14
Bernard Williams <bernard.l.williams@live.com>
2017-05-20 00:40:22 -0400

    Initial changes

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -2,4 +2,28 @@
 Bernard Williams
 UCF Bootcamp APR2017 Week 4
 RPG Starwars Game
-*/ 
+*/
+
+//Characters array holding all of the game characters
+var characters = [
+  {
+    name: "Darth Vader",
+    imgURL: "",
+    desc: ""
+  },
+  {
+    name: "Hans Solo",
+    imgURL: "",
+    desc: ""
+  },
+  {
+    name: "Kylo-Ren",
+    imgURL: "",
+    desc: ""
+  },
+  {
+    name: "R2D2",
+    imgURL: "",
+    desc: ""
+  }
+];
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,3 +3,7 @@ Bernard Williams
 UCF Bootcamp APR2017 Week 4
 RPG Starwars Game
 */ 
+
+function gameInit () {
+    
+}
