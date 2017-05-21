4c51faf4ffeb2e12120d7b8f9823865f68de22a5
Bernard Williams <bernard.l.williams@live.com>
2017-05-21 11:51:50 -0400

    significant progress

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -4,30 +4,39 @@ UCF Bootcamp APR2017 Week 4
 RPG Starwars Game
 */
 
-//Characters array holding all of the game characters
+//Characters array holding all of the game characters as objects
 var characters = [
   {
     name: "Darth Vader",
     imgURL: "assets/images/darthVader.jpg",
-    desc: "This Captain of Chaos with a bad respiratory issue will never leave yhou wanting for less destruction.  Luke... I am your father!",
-    id: "dv"
+    desc: "This Captain of Chaos with a bad respiratory issue will never leave you wanting for less destruction.  Luke... I am your father!",
+    id: "dv",
+    damage: 0,
+    hitPoints: 0
   },
   {
     name: "Hans Solo",
     imgURL: "assets/images/hans-solo.jpg",
-    desc: "Suave... Debonaire... and he got to make out with Carrie Fisher!!  Chewy was the heavy though.",
-    id: "hs"
+    desc: "Suave... Debonair... and he got to make out with Carrie Fisher!!  Chewy was the heavy though.",
+    id: "hs",
+    damage: 0,
+    hitPoints: 0
   },
   {
     name: "Kylo-Ren",
     imgURL: "assets/images/kylo-ren.jpg",
     desc: "Angst, angst, angst!!!  This son of a princess needs a week with Serina Williams to set her straight",
-    id: "kr"
+    id: "kr",
+    damage: 0,
+    hitPoints: 0
   },
   {
     name: "R2D2",
     imgURL: "assets/images/r2d2.jpg",
     desc: "Part machine, part asshole.  This trashcan look alike has a lot of attitude and ALWAYS saves the day.",
-    id: "rs"
+    id: "rs",
+    damage: 0,
+    hitPoints: 0
   }
 ];
+
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -2,16 +2,38 @@
 Bernard Williams
 UCF Bootcamp APR2017 Week 4
 RPG Starwars Game
-*/ 
-
-
+*/
 
 //Variable Definition
-var characterSection = 
+var characterSection = function gameInit() {
+  //Reset the characters
+  //Reread the config file
+};
 
+//Function used to filter the array of players to only return those players not selected
+function getEnemies(player) {
+  console.log("------------------------------------");
+  console.log("get enemies ran " + player);
+  console.log(player);
+  console.log("------------------------------------");
+  if (player.id != currentPlayerID){
+      enemies.push(player);
+  } else {
+      currentPlayer = player;
+  }
+}
 
-function gameInit () {
-    //Reset the characters
+//Function to randomly generate a players initial strength
+function getHitPoints(){
 
-    //Reread the config file
+}
+
+//Function that facilitates the players attack of the defender
+function playerAttack(){
+
+}
+
+//Function that facilitates the players attack of the defender
+function defenderResponse(){
+
 }
