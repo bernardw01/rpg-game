a86dc6f67879e84208786e8755e853bfc165f25b
Bernard Williams <bernard.l.williams@live.com>
2017-05-20 21:53:14 -0400

    First major revision

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -8,22 +8,26 @@ RPG Starwars Game
 var characters = [
   {
     name: "Darth Vader",
-    imgURL: "",
-    desc: ""
+    imgURL: "assets/images/darthVader.jpg",
+    desc: "This Captain of Chaos with a bad respiratory issue will never leave yhou wanting ofr less destruction.  Luke... I am your father!",
+    id: "dv"
   },
   {
     name: "Hans Solo",
     imgURL: "",
-    desc: ""
+    desc: "Suave... Debonaire... and he got to make out with Carrie Fisher!!  Chewy was the heavy though.",
+    id: "hs"
   },
   {
     name: "Kylo-Ren",
     imgURL: "",
-    desc: ""
+    desc: "Angst, angst, angst!!!  This son of a princess needs a week with Serina Williams to set him straight",
+    id: "kr"
   },
   {
     name: "R2D2",
     imgURL: "",
-    desc: ""
+    desc: "Part machine, part asshole.  This trashcan look alike has a lot of attitude and ALWAYS saves the day.",
+    id: "rs"
   }
 ];
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -4,6 +4,14 @@ UCF Bootcamp APR2017 Week 4
 RPG Starwars Game
 */ 
 
+
+
+//Variable Definition
+var characterSection = 
+
+
 function gameInit () {
-    
+    //Reset the characters
+
+    //Reread the config file
 }
